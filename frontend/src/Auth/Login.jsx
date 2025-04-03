8094bb27e7c612ef4044a2685d0e34514180c3da
alfaxad1 <alfaxadder@gmail.com>
2025-04-03 23:55:50 +0300

    login

--- frontend/src/Auth/Login.jsx
+++ frontend/src/Auth/Login.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [values, setValues] = useState({ password: "", username: "" });
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <form id="login-form">
-        <div class="input-group">
-          <i class="fas fa-user"></i>
-          <input type="text" id="username" placeholder="Username" required />
-        </div>
-        <div class="input-group">
-          <i class="fas fa-lock"></i>
+        <div>
           <input
-            type="password"
-            id="password"
-            placeholder="Password"
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={(e) => handleChange(e)}
             required
           />
         </div>
-        <div class="register-link"></div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => handleChange(e)}
+            required
+          />
+        </div>
+        {/* <div></div> to register */}
         <button type="submit">Login</button>
       </form>
     </>
