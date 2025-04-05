1e77bd5f4182e8f591c05aca2d1298fc94533911
alfaxad1 <alfaxadder@gmail.com>
2025-04-06 00:41:42 +0300

    dashboard grid

--- frontend/src/home/Dashboard.jsx
+++ frontend/src/home/Dashboard.jsx
@@ -3,15 +3,45 @@ import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div>
-      This is the Dashboard
-      <Link to={`/purchases`}> Purchases</Link>
-      <Link to={`/sales`}> Sales</Link>
-      <Link to={`/expenses`}> Expenses</Link>
-      <Link to={`/chicken-purchases`}> Chicken Purchases</Link>
-      <Link to={`/chicken-loss`}> Loss</Link>
-      <Link to={`/eggs-collection`}> Egg collection</Link>
-    </div>
+    <>
+      <div className="grid grid-cols-3 gap-x-10 gap-y-3 m-6">
+        <Link to={`/purchases`}>
+          <div className="border-black bg-red-500 rounded-lg shadow-xl text-indigo-800 font-bold text-center text-xl p-10 min-h-[100px]">
+            Purchases
+          </div>
+        </Link>
+
+        <Link to={`/sales`}>
+          <div className="border-black bg-slate-500 rounded-lg shadow-xl text-indigo-800 font-bold text-center text-xl p-10 min-h-[100px]">
+            Sales
+          </div>
+        </Link>
+
+        <Link to={`/expenses`}>
+          <div className="border-black bg-yellow-500 rounded-lg shadow-xl text-indigo-800 font-bold text-center text-xl p-10 min-h-[100px]">
+            Expenses
+          </div>
+        </Link>
+
+        <Link to={`/chicken-purchases`}>
+          <div className="border-black bg-teal-500 rounded-lg shadow-xl text-indigo-800 font-bold text-center text-xl p-10 min-h-[100px]">
+            Chicken purchases
+          </div>
+        </Link>
+
+        <Link to={`/chicken-loss`}>
+          <div className="border-black bg-purple-500 rounded-lg shadow-xl text-indigo-800 font-bold text-center text-xl p-10 min-h-[100px]">
+            Loss
+          </div>
+        </Link>
+
+        <Link to={`/eggs-collection`}>
+          <div className="border-black bg-indigo-500 rounded-lg shadow-xl text-indigo-800 font-bold text-center text-xl p-10 min-h-[100px]">
+            Eggs collection
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
